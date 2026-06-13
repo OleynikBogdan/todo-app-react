a882ae1a192d867372b584196b065fac6241f991
SaburoDarame <profenyx@gmail.com>
2026-06-13 16:05:56 +0300

    Implement todo completion functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,81 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const App = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const completedTasks = tasks.filter((item) => item.complete).length;
 
-    const [task, setTask] = useState("");
-    const [tasks, setTasks] = useState([]);
-    
-function addTask() {
-if (!task.trim()) {
-  return;
-}
-
-const newTask = {
-id: Date.now(),
-text: task,
-complete: false,
-
-}
-setTasks([...tasks, newTask]);
-setTask("");
-}
-
-
-function deleteTask(idToDelete) {
-  const updatedTasks = tasks.filter(
-    (item) => item.id !==idToDelete 
-  );
-
-  setTasks(updatedTasks);
-
-}
-
-function toggleTask(id) {
-  const updatedTasks = tasks.map((item)=>{ 
-    if(item.id === id){
-      return {
-        ...item,
-        complete: !item.complete,
-      };
+  function addTask() {
+    if (!task.trim()) {
+      return;
     }
-    return item
-  });
-  return setTasks(updatedTasks)
-}
 
+    const newTask = {
+      id: Date.now(),
+      text: task,
+      complete: false,
+    };
+    setTasks([...tasks, newTask]);
+    setTask("");
+  }
+
+  function deleteTask(idToDelete) {
+    const updatedTasks = tasks.filter((item) => item.id !== idToDelete);
+
+    setTasks(updatedTasks);
+  }
+
+  function toggleTask(id) {
+    const updatedTasks = tasks.map((item) => {
+      if (item.id === id) {
+        return {
+          ...item,
+          complete: !item.complete,
+        };
+      }
+      return item;
+    });
+    return setTasks(updatedTasks);
+  }
 
   return (
     <div>
-    <input 
-    type="text"
-    value={task}
-    onChange={(e) => setTask(e.target.value)}
-    />
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
 
-<button onClick={addTask}> Add</button>
+      <button onClick={addTask}> Add</button>
 
-    <ul>
-      {tasks.map((item) => (
-        <li key={item.id} style={{ 
-          textDecoration: item.complete ? "line-through": "none", 
-          opacity: item.complete ? 0.5 : 1,
+      <ul>
+        {tasks.map((item) => (
+          <li
+            key={item.id}
+            style={{
+              textDecoration: item.complete ? "line-through" : "none",
+              opacity: item.complete ? 0.5 : 1,
+            }}
+          >
+            <input
+              type="checkbox"
+              checked={item.complete}
+              onChange={() => {
+                toggleTask(item.id);
+              }}
+            />
+            {item.text}
 
-        }}>
-<input type="checkbox" checked={item.complete} onChange={()=> {toggleTask(item.id)}}/>
-          {item.text}
-        
-        <button onClick={()=> deleteTask(item.id)}> 
-          Delete
-        </button>
-        </li>
-      ))}
-    </ul>
+            <button onClick={() => deleteTask(item.id)}>Delete</button>
+          </li>
+        ))}
+
+        <p>
+          Completed {completedTasks} of {tasks.length} tasks
+        </p>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
